ba8782e34675fb00b5098f0ad3519179aafaced0
fauzia <fauzia.durrani@clustox.com>
2025-08-11 05:04:18 +0500

    smash the candies

---
+++ CandySmash.py
@@ -0,0 +1,23 @@
+def to_smash(total_candies):
+    """Return the number of leftover candies that must be smashed after distributing
+    the given number of candies evenly between 3 friends.
+
+     to_smash(91)
+    1
+    """
+    if total_candies < 2:
+        # print("Splitting", total_candies, "candy")
+        print("Splitting 1 candy")
+        return total_candies % 3
+    else:
+        print("Splitting", total_candies, "candies")
+        return total_candies % 3
+
+
+to_smash(91)
+to_smash(1)
+
+#when called with print, returned value will be shown as well
+print ("calling in print")
+print(to_smash(91))
+print(to_smash(5))
